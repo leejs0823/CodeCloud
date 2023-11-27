f024c5a41f4f79da0649f99238647c5f27eb82a4
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-11-28 03:50:29 +0900

    :recycle: Reafctor : group model comment
    reafctor : group model comment

--- src/main/java/model/Group.java
+++ src/main/java/model/Group.java
@@ -1,66 +1,73 @@
 package model;
 
 
-// Group Class
+//Group 클래스는 그룹의 정보를 나타냄 
 public class Group {
-    private Long groupId;
-    private String groupName;
-    private String description;
-    private String groupImage; // Assuming the image is stored as a path or URL string
+ private Long groupId; // 그룹의 고유 식별자
+ private String groupName; // 그룹 이름
+ private String description; // 그룹 설명
+ private String groupImage; // 그룹 이미지의 경로 또는 URL을 가정한 문자열
 
-    // Default constructor
-    public Group() {
-    }
+ // 기본 생성자
+ public Group() {
+ }
 
-    // Parameterized constructor
-    public Group(Long groupId, String groupName, String description, String groupImage) {
-        this.groupId = groupId;
-        this.groupName = groupName;
-        this.description = description;
-        this.groupImage = groupImage;
-    }
+ // 매개변수가 있는 생성자
+ public Group(Long groupId, String groupName, String description, String groupImage) {
+     this.groupId = groupId;
+     this.groupName = groupName;
+     this.description = description;
+     this.groupImage = groupImage;
+ }
 
-    // Getters and setters
-    public Long getGroupId() {
-        return groupId;
-    }
+ // groupId 필드의 getter 메소드
+ public Long getGroupId() {
+     return groupId;
+ }
 
-    public void setGroupId(Long groupId) {
-        this.groupId = groupId;
-    }
+ // groupId 필드의 setter 메소드
+ public void setGroupId(Long groupId) {
+     this.groupId = groupId;
+ }
 
-    public String getGroupName() {
-        return groupName;
-    }
+ // groupName 필드의 getter 메소드
+ public String getGroupName() {
+     return groupName;
+ }
 
-    public void setGroupName(String groupName) {
-        this.groupName = groupName;
-    }
+ // groupName 필드의 setter 메소드
+ public void setGroupName(String groupName) {
+     this.groupName = groupName;
+ }
 
-    public String getDescription() {
-        return description;
-    }
+ // description 필드의 getter 메소드
+ public String getDescription() {
+     return description;
+ }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+ // description 필드의 setter 메소드
+ public void setDescription(String description) {
+     this.description = description;
+ }
 
-    public String getGroupImage() {
-        return groupImage;
-    }
+ // groupImage 필드의 getter 메소드
+ public String getGroupImage() {
+     return groupImage;
+ }
 
-    public void setGroupImage(String groupImage) {
-        this.groupImage = groupImage;
-    }
+ // groupImage 필드의 setter 메소드
+ public void setGroupImage(String groupImage) {
+     this.groupImage = groupImage;
+ }
 
-    // toString method for easy printing and debugging
-    @Override
-    public String toString() {
-        return "Group{" +
-                "groupId=" + groupId +
-                ", groupName='" + groupName + '\'' +
-                ", description='" + description + '\'' +
-                ", groupImage='" + groupImage + '\'' +
-                '}';
-    }
+ // 객체의 문자열 표현을 반환하는 toString 메소드
+ @Override
+ public String toString() {
+     return "Group{" +
+             "groupId=" + groupId +
+             ", groupName='" + groupName + '\'' +
+             ", description='" + description + '\'' +
+             ", groupImage='" + groupImage + '\'' +
+             '}';
+ }
 }
